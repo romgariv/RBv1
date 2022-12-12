8ee48c393d931f47ca9f4fbd332725259a1d6fd3
romgariv <romgariv@gmail.com>
2022-12-12 17:58:16 +0300

    updated with sterilized

--- chicago/format/TRR-weapon-discharges_2004-2018_2018-08/src/format.py
+++ chicago/format/TRR-weapon-discharges_2004-2018_2018-08/src/format.py
@@ -10,7 +10,7 @@ from general_utils import FormatData
 def get_setup():
     script_path = __main__.__file__
     args = {
-        'input_file' : 'input/P456008 13094-FOIA-P456008-TRRdata Responsive Record Produced By R&A.xlsx',
+        'input_file' : 'input/P456008 13094-FOIA-P456008-TRRdata Responsive Record Produced By R&A_sterilized.xlsx',
         'output_file' : 'output/TRR-weapon-discharges_2004-2018_2018-08.csv.gz',
         'sheet' : 'WeaponDischarges',
         'column_names_key': 'TRR-weapon-discharges_2004-2018_2018-08'
